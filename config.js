6347654aa4994fd38247e0a50de00a42e7ad08c1
Leonidk29 <leonid.rossenkov@gmail.com>
2018-05-11 23:56:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const config = {};
 config.plotly={};
 
 
-config.mongo_url = 'mongodb://meteringuser:meters@ds135817.mlab.com:35817/metering-test-project';
+config.mongo_url = 'secreturl';
 config.plotly.name = 'letoll';
-config.plotly.token = 'vvNrj7MzDRhx1YLndIfi';
+config.plotly.token = 'token';
 config.secret = 'Secretly Secret';
 config.graph_days = 60; // Consumption query where createdAt>Date.now-graph_days
 config.worker_interval = 1000*60; //Delay between worker requests ms
